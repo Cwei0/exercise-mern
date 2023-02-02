ca6f878b139da2648ebd8351775045c361403741
Cwei0 <bottgabriel@icloud.com>
2023-02-02 22:30:22 +0100

    connected front to back

--- Server/server.js
+++ Server/server.js
@@ -3,19 +3,20 @@ const express = require('express')
 
 const app = express();
 const mongoose = require('mongoose')
-const PORT = process.env.PORT || 5000;
 
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const connectDB = require('./config/dbConn')
+const cors = require('cors')
 
+const PORT = process.env.PORT || 8000;
 //Mongoose middleware
 connectDB()
 
 //Logging Events
 app.use(logger)
 
-
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+
+    const [workout, setWorkout] = useState(null)
+
+    useEffect(() => {
+
+        const fetchWorkout = async() => {
+            const responce = await fetch('http://localhost:8000/workout')
+            const data = await responce.json()
+           
+            try {
+                if(responce.ok) {
+                    setWorkout(data)
+                    console.log('success')
+                }
+            } catch (error) {
+                console.error(error)
+            }
+           
+        }
+        fetchWorkout()
+    }, [])
     
+
     return (
-        <div className="Home">
-            <h2>Home</h2>
+        <div className="home">
+            {workout && workout.map((workout) => (
+                <p key={workout._id}>
+                    {workout.title}
+                </p>
+            ))}
         </div>
     );
 }
